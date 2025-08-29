097ed6969139cb293b6f45e25163801804ee81c2
adarsh06s <adarshps51109@gmail.com>
2025-08-29 20:11:41 +0530

    first commit

---
+++ Area_of_rectangle.py
@@ -0,0 +1,5 @@
+# Program to find area of rectangle
+l = float(input('Enter the length :'))
+b = float(input('Enter the breadth :'))
+area = l*b
+print('Area of rectangle :',area)
---
+++ Hello_world.py
@@ -0,0 +1,10 @@
+# Displaying the hello world
+
+print('Hello world')    # we can use single quotes
+print("Hello world")    # we can use double quotes
+
+# Program to values of variables 
+A = "adarsh"    # Storing the string (adarsh) in the variable 'A'
+N = 100         # Storing the value(100) in the variable 'N'
+print(A)
+print(N)
